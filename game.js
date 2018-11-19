96a97728ac32dc85c0f32534dcc9afa3e0000081
Augustinraj.L <45088999+augustin1998@users.noreply.github.com>
2018-11-19 20:45:07 +0530

    Update game.js

--- game.js
+++ game.js
@@ -12,15 +12,15 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         var dice1 = Math.floor(Math.random() * 6) + 1;
         var dice2 = Math.floor(Math.random() * 6) + 1;
 
-        // Display the result
+        //  result display
         document.getElementById('dice-1').style.display = 'block';
         document.getElementById('dice-2').style.display = 'block';
         document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
         document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
-        // Update the round score IF the rolled number was NOT a 1
+       
         if (dice1 !== 1 && dice2 !== 1) {
-            //Add score
+            
             roundScore += dice1 + dice2;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
         } else {
@@ -35,7 +35,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
     if (gamePlaying) {
-        // Adding  CURRENT score to GLOBAL score
+        
         scores[activePlayer] += roundScore;
 
         // Updating  the UI
